2db75df8077ba14a20ff3e26a05804448b25f7b1
Colby Janecka <colbyjanecka@aol.com>
2019-09-05 17:04:36 -0500

    Added Proper commentation

--- src/assignment1/Problem1.java
+++ src/assignment1/Problem1.java
@@ -1,3 +1,5 @@
+/* Student Name: Colby Janecka,  Lab Section: Unique #16175 */
+
 package assignment1;
 import java.io.IOException;
 import java.math.BigInteger;
@@ -5,48 +7,48 @@ import java.util.Scanner;
 
 public class Problem1 {
     public static void main(String[] args) throws IOException {
-        Scanner inputScanner = new Scanner(System.in);
+        Scanner inputScanner = new Scanner(System.in);                          // Creates scanner for getting inputs
 
         System.out.print("Enter a number of adjacent digits to multiply: ");
-        int digitAmount = inputScanner.nextInt();
+        int digitAmount = inputScanner.nextInt();                              // Gets number of adjacent digits
 
-        while (digitAmount > 20 || digitAmount < 2){
+        while (digitAmount > 20 || digitAmount < 2){                            // confirms that 2<=n<=20
             System.out.print("Range must be between 2 and 20: ");
             digitAmount = inputScanner.nextInt();
         }
 
         System.out.print("Enter string of 1000 digits: ");
-        String inputString = inputScanner.next();
+        String inputString = inputScanner.next();                               // Gets string of 1000 digits
 
-        while(inputString.length() != 1000){
+        while(inputString.length() != 1000){                                    // Confirms that s.length() = 1000
             System.out.print("Incorrect amount of digits : ");
             inputString = inputScanner.next();
         }
-        BigInteger inputNumber = new BigInteger(inputString);
+        BigInteger inputNumber = new BigInteger(inputString);                   // Creates BigInteger from input string
 
         int num = 0;
         int largest = 0;
         int currentTotal = 1;
 
-        while(inputNumber.intValue() != 0){
+        while(inputNumber.intValue() != 0){                                     // loop that runs till end of #
 
             BigInteger currentBigInt = inputNumber;
 
             currentTotal = 1;
 
-            for(int i = digitAmount; i>0; i--) {
-                num = currentBigInt.mod(BigInteger.valueOf(10)).intValue();
+            for(int i = digitAmount; i>0; i--) {                                // loop that runs n times
+                num = currentBigInt.mod(BigInteger.valueOf(10)).intValue();     //   and multiplies n adjacent numbers
                 currentTotal *= num;
                 currentBigInt = currentBigInt.divide(BigInteger.valueOf(10));
             }
 
-            if(currentTotal > largest){
-                largest = currentTotal;
+            if(currentTotal > largest){                                         // largest product is replaced
+                largest = currentTotal;                                         //    if needed
             }
 
-            inputNumber = inputNumber.divide(BigInteger.valueOf(10));
+            inputNumber = inputNumber.divide(BigInteger.valueOf(10));           // truncate last digit off BigInteger
         }
 
-        System.out.println("Largest digit: \n" + largest);
+        System.out.println("Largest digit: \n" + largest);                      // print and return result
     }
 }
