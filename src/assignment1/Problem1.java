9880e5ce239b3146e90ab6eb3331c058ccb8d55c
Colby Janecka <colbyjanecka@aol.com>
2019-09-05 15:39:18 -0500

    first commit

---
+++ src/assignment1/Problem1.java
@@ -0,0 +1,11 @@
+package assignment1;
+import java.util.Scanner;
+
+public class Problem1 {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter a fucking number:");
+        String firstName = myObj.nextLine();
+        System.out.println(firstName + " says sup");
+    }
+}
