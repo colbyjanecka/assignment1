4ce8995c785ae0741365354ea82b9ed17cdfa5f9
Colby Janecka <colbyjanecka@aol.com>
2019-09-12 20:16:13 -0500

    Finished project.

--- src/assignment1/Problem3.java
+++ src/assignment1/Problem3.java
@@ -1,14 +1,13 @@
 /* Student Name: Colby Janecka,  Lab Section: Unique #16175 */
 
 package assignment1;
-import java.util.Scanner;
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 
 public class Problem3 {
 
     public static String problem3(String s){
 
-        MaxentTagger tagger = new MaxentTagger("taggers/english-left3words-distsim.tagger");
+        MaxentTagger tagger = new MaxentTagger("tagger/english-left3words-distsim.tagger");
                                                                         // Generate new tagger from stanford library
         String taggedInput = tagger.tagString(s).trim();                // tag and trim string provided
 
